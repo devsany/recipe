bbc6561cda2aef5182c8d217346a59cbe571e0f5
devsany <sunnyrajbodhgaya19@gmail.com>
2024-10-07 22:52:40 +0530

    add recipi highlight

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,76 @@
+import { useUser } from "@clerk/clerk-react";
+import { ChevronRight } from "lucide-react";
+import { Link, NavLink } from "react-router-dom";
+import RecipeHighlight from "./homeComponent/RecipeHighlight";
+
 const Home = () => {
-  return <div>Home</div>;
+  const { user } = useUser();
+  return (
+    <div>
+      <div className="  w-[100%] bg-gradient-to-b from-yellow-200 h-[100vh]">
+        <div className="grid     md:grid-cols-2 md:pt-16">
+          <div className="flex justify-left items-center">
+            <div className="ml-7 pt-10 md:ml-10 mr-10 items-center ">
+              <h2 className="text-3xl pb-2 font-bold bg-gradient-to-r from-yellow-500 to-red-600 bg-clip-text text-transparent scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+                Furniture That Fits Your Life, Style, and Space
+              </h2>
+
+              <div className="leading-7 font-thin   [&:not(:first-child)]:mt-6">
+                At{" "}
+                <span className="bg-gradient-to-l text-white text-bold pb-1 pt-1 rounded pl-1 pr-1 from-yellow-400 to-red-500">
+                  {" "}
+                  Recipe Book
+                </span>{" "}
+                , we create furniture that enhances your living space. Our
+                collection combines quality, design, and comfort, ensuring every
+                piece feels right at home. Discover how our furniture can
+                transform your everyday moments into extraordinary experiences
+              </div>
+              <div className="mt-5">
+                {user ? (
+                  <>
+                    <button className="border  border-red-300 shadow-md shadow-yellow-200">
+                      <NavLink to="/write_recipe_book">
+                        <div className="flex">
+                          <div>Get Started </div>
+                          <div className="pt-[2px]">
+                            <ChevronRight className="ml-1 border-none h-4 w-4" />
+                          </div>
+                        </div>
+                      </NavLink>
+                    </button>
+                  </>
+                ) : (
+                  <>
+                    <button className="border  border-red-300 shadow-md shadow-yellow-200">
+                      <Link to="https://leading-wallaby-6.accounts.dev/sign-in#/?redirect_url=http%3A%2F%2Flocalhost%3A5174%2F">
+                        <div className="flex">
+                          <div>Get Started </div>
+                          <div className="pt-[2px]">
+                            <ChevronRight className="ml-1 border-none h-4 w-4" />
+                          </div>
+                        </div>
+                      </Link>
+                    </button>
+                  </>
+                )}
+              </div>
+            </div>
+          </div>
+          <div>
+            <img
+              className="w-[600px] rounded-lg shadow-lg lg:p-5 md:p-4 sm:p-5 p-5"
+              src="4.png"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="flex mt-[24vh] justify-center">
+          <RecipeHighlight />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/components/homeComponent/RecipeHighlight.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import { recipe_highlight } from "../JSON/data";
+
+const RecipeHighlight = () => {
+  const [data, setData] = useState(recipe_highlight);
+  return (
+    <div>
+      {" "}
+      <div className="flex mt-2 mb-10 md:mt-[20vh] justify-center">
+        <h2 className="text-3xl  font-serif font-bold border-l-lime-500-4">
+          Recipe Highlights
+        </h2>
+      </div>
+      <div className="grid md:grid-cols-3 items-stretch sm:grid-cols-1 p-10">
+        {data &&
+          data.map((item, index) => {
+            return (
+              <>
+                <div className="m-6 border" key={index}>
+                  <div className="flex justify-between">
+                    <div className="bg-green-200 m-3 text-sm text-slate-800 pl-2 pr-2 rounded-xl">
+                      {item.type}
+                    </div>
+                    <div className="bg-yellow-200 m-3 text-sm text-slate-800 pl-2 pr-2 rounded-xl">
+                      {item.version}
+                    </div>
+                  </div>
+                  <div className="flex flex-wrap  justify-center">
+                    <img
+                      className="shadow-lg m-3 h-[150px]"
+                      src={item.img}
+                      alt=""
+                    />
+                  </div>
+                  <div className="text-gray-700 font-semibold flex justify-center text-xl font-serif">
+                    {item.title}
+                  </div>
+                  <div className="pl-2 pr-2 pb-3 text-slate-700">
+                    {item.description.slice(0, 125) + " ..."}
+                  </div>
+                </div>
+              </>
+            );
+          })}
+      </div>
+    </div>
+  );
+};
+
+export default RecipeHighlight;
